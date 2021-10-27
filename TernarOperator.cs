41f2032f5f5fe63074ee27a0c813e93efc8baaa2
VladimirPetrosyan <92800312+VladimirPetrosyan@users.noreply.github.com>
2021-10-27 13:17:47 +0300

    Add files via upload

---
+++ TernarOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TernarOperator
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            int a, b, c, m;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            m=(a < b && a<c) ? (b < c && b<a) ? a : b : c;
+            Console.WriteLine(m);
+            Console.ReadKey();
+        }
+    }
+}
